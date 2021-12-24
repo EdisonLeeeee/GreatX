0094670d20686cd1166ac985b824a906fd0e92ce
EdisonLeeeee <cnljt@outlook.com>
2021-12-24 15:42:14 +0800

    update drop_edge and drop_node

--- graphwar/functional/dropouts.py
+++ graphwar/functional/dropouts.py
@@ -6,14 +6,19 @@ def drop_edge(g, p=0.5, training=True):
     DropEdge: Sampling edge using a uniform distribution.
     """
     
+    if p < 0. or p > 1.:
+        raise ValueError(f'Dropout probability has to be between 0 and 1 '
+                         f'(got {p}')    
+    
     if not training or not p:
         return g
 
     g = g.local_var()
     num_edges = g.num_edges()
-    num_drops = int(p * num_edges)
-    perm = torch.randperm(num_edges, device=g.device)
-    g.remove_edges(perm[:num_drops])
+    e_ids = torch.arange(num_edges, dtype=torch.long, device=g.device)
+    mask = torch.full_like(e_ids, p, dtype=torch.float32)
+    mask = torch.bernoulli(mask).to(torch.bool)
+    g.remove_edges(e_ids[mask])
     return g
 
 
@@ -22,11 +27,15 @@ def drop_node(g, p=0.5, training=True):
     DropNode: Sampling node using a uniform distribution.
     """
     
+    if p < 0. or p > 1.:
+        raise ValueError(f'Dropout probability has to be between 0 and 1 '
+                         f'(got {p}')    
+        
     if not training or not p:
         return g
 
     num_nodes = g.num_nodes()
-    nodes = torch.arange(num_nodes, dtype=torch.long)
+    nodes = torch.arange(num_nodes, dtype=torch.long, device=g.device)
     mask = torch.full_like(nodes, 1 - p, dtype=torch.float32)
     mask = torch.bernoulli(mask).to(torch.bool)
     subset = nodes[mask]

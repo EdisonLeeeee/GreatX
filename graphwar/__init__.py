9df700dab09bb63d7a4e65f3be383665dc7b702f
EdisonLeeeee <cnljt@outlook.com>
2022-01-04 15:57:05 +0800

    fix import error

--- graphwar/__init__.py
+++ graphwar/__init__.py
@@ -1,8 +1,8 @@
-from . import attack, data, defense, functional, models, nn, training, utils
 from .config import Config
-from .surrogater import Surrogater
 from .utils.seed import set_seed
 from .version import __version__
+from .surrogater import Surrogater
+from . import attack, data, defense, functional, models, nn, training, utils
 
 __all__ = ['__version__', 'Config', 'set_seed', 'Surrogater',
            'data', 'attack', 'defense',
--- graphwar/training/__init__.py
+++ graphwar/training/__init__.py
@@ -1,4 +1,4 @@
 from . import callbacks
+from .trainer import Trainer
 from .get_trainer import get_trainer
 from .robustgcn_trainer import RobustGCNTrainer
-from .trainer import Trainer

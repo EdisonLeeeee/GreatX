838567e24cbce24a3dae7f91246ffa7fa72222ca
EdisonLeeeee <cnljt@outlook.com>
2022-07-03 18:25:07 +0800

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,26 +1,5 @@
 import datetime
-import doctest
-
-import sphinx_rtd_theme
-
 import greatx
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-]
-
-autosummary_generate = True
-templates_path = ['_templates']
-
-source_suffix = '.rst'
-master_doc = 'index'
 
 author = 'Jintang Li'
 project = 'GreatX'
@@ -29,43 +8,33 @@ copyright = '{}, {}'.format(datetime.datetime.now().year, author)
 version = greatx.__version__
 release = greatx.__version__
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
-doctest_default_flags = doctest.NORMALIZE_WHITESPACE
-autodoc_member_order = 'bysource'
-intersphinx_mapping = {'python': ('https://docs.python.org/', None)}
-
-html_theme_options = {
-    'collapse_navigation': False,
-    'display_version': True,
-    'logo_only': False,
-    'navigation_depth': 4,
-}
-
+html_theme = 'pyg_sphinx_theme'
 html_logo = '../../imgs/greatx.png'
-html_static_path = ['_static']
-# html_context = {'css_files': ['_static/css/custom.css']}
-rst_context = {'greatx': greatx}
+html_favicon = '../../imgs/greatx.png'
 
 add_module_names = False
+autodoc_member_order = 'bysource'
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy', None),
+    'pandas': ('http://pandas.pydata.org/pandas-docs/dev', None),
+    'torch': ('https://pytorch.org/docs/master', None),
+}
 
 
 def setup(app):
-    def skip(app, what, name, obj, skip, options):
-        members = [
-            '__init__',
-            '__repr__',
-            '__weakref__',
-            '__dict__',
-            '__module__',
-        ]
-        return True if name in members else skip
+    def rst_jinja_render(app, _, source):
+        rst_context = {'greatx': greatx}
+        source[0] = app.builder.templates.render_string(source[0], rst_context)
 
-    def rst_jinja_render(app, docname, source):
-        src = source[0]
-        rendered = app.builder.templates.render_string(src, rst_context)
-        source[0] = rendered
-
-    app.connect('autodoc-skip-member', skip)
-    app.connect("source-read", rst_jinja_render)
+    app.connect('source-read', rst_jinja_render)

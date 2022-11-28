a49bf6d7ae751d685a302ce75949c509731a96ad
EdisonLeeeee <cnljt@outlook.com>
2022-11-28 10:24:01 +0800

    import

--- greatx/attack/untargeted/__init__.py
+++ greatx/attack/untargeted/__init__.py
@@ -5,8 +5,15 @@ from .metattack import Metattack
 from .pgd_attack import PGDAttack
 from .random_attack import RandomAttack
 from .untargeted_attacker import UntargetedAttacker
+from .rbcd_attack import PRBCDAttack
 
 classes = __all__ = [
-    'UntargetedAttacker', 'RandomAttack', 'DICEAttack', 'FGAttack', 'IGAttack',
-    'Metattack', 'PGDAttack'
+    'UntargetedAttacker',
+    'RandomAttack',
+    'DICEAttack',
+    'FGAttack',
+    'IGAttack',
+    'Metattack',
+    'PGDAttack',
+    'PRBCDAttack',
 ]

2ca9add9cebfb67d109c779d6ebd52dea865aeea
EdisonLeeeee <cnljt@outlook.com>
2023-03-16 18:00:08 +0800

    fix circular imports

--- greatx/training/__init__.py
+++ greatx/training/__init__.py
@@ -1,8 +1,10 @@
-from . import callbacks
+# isort: skip
+
+from . import callbacks  # isort: skip
+from .trainer import Trainer  # isort: skip
+from .unsup_trainer import UnspuervisedTrainer  # isort: skip
 from .get_trainer import get_trainer
 from .sat_trainer import SATTrainer
-from .trainer import Trainer
-from .unsup_trainer import UnspuervisedTrainer
 
 classes = __all__ = [
     "Trainer",
